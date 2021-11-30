e0a1a341bce33c78e7d9df5e397a2f7e781f1698
Susanna Yossef <susannayossef98@gmail.com>
2021-11-30 17:38:02 +0100

    update

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@ function printMessage(msg){
 	var div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
+    var computerMove;
+    computerMove = 'kamień';
+    printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+    var computerMove;
+    computerMove = 'papier';
+    printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 }
 
 function clearMessages(){
