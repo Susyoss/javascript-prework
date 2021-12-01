dd17ddfae4e3565c20e5ee5e2f663b683f9e3219
Susanna Yossef <susannayossef98@gmail.com>
2021-12-01 13:03:45 +0100

    update

--- js/script.js
+++ js/script.js
@@ -5,9 +5,18 @@ function printMessage(msg){
     var computerMove;
     computerMove = 'kamień';
     printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-    var computerMove;
-    computerMove = 'papier';
-    printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+    var playerMove;
+    playerMove = 'papier';
+    printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+    var randomNumber;
+    randomNumber = Math.floor(Math.random() * 3 + 1);
+    printMessage('Wylosowana liczba to: ' + randomNumber);
+    if (randomNumber == '1') {
+        computerMove = 'kamień';
+    } else {
+        computerMove = 'nieznany ruch';
+    }
+      printMessage('Mój ruch: ' + computerMove);
 }
 
 function clearMessages(){
